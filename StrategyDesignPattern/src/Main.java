d7f93795ecd741434c0353d1cd9de8b0dd6c3703
Elad Shamailov <eladsham@post.bgu.ac.il>
2017-12-11 21:55:55 +0200

    Add files via upload

--- StrategyDesignPattern/src/Main.java
+++ StrategyDesignPattern/src/Main.java
@@ -5,7 +5,10 @@ public class Main {
         Animal shuki = new Dog("shuki",60,30); //creates new dog
         Animal mikey = new Mouse("mikey",10,4); //creates new mouse
         Animal roni = new Shark("roni",26,10); //creates new shark
-
+        System.out.println("I'm a dog and my name is: "+shuki.getName());
+        System.out.println("I'm a mouse and my name is: "+mikey.getName());
+        System.out.println("I'm a shark and my name is: "+roni.getName());
+        System.out.println();
         System.out.println("Dog: " + shuki.tryToRun());
         System.out.println("Mouse: " + mikey.tryToRun());
         System.out.println("Shark: " + roni.tryToRun());
